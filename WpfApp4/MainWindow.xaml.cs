8c4bafe6c71552997f0d6503db17c329c2e61827
MirtalibEmirli <azad.sdk.85@icloud.com>
2024-04-24 14:50:10 +0400

    Add project files.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp4;
+
+namespace WpfDatabinding
+{
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty MessagesProperty =
+            DependencyProperty.Register("Messages", typeof(ObservableCollection<Message1>), typeof(MainWindow), new PropertyMetadata(null));
+
+        public ObservableCollection<Message1> Messages
+        {
+            get { return (ObservableCollection<Message1>)GetValue(MessagesProperty); }
+            set { SetValue(MessagesProperty, value); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadMessages();
+            DataContext = this;
+        }
+
+        private void LoadMessages()
+        {
+            
+                string datas = File.ReadAllText("../../../msg.json");
+                Messages = JsonSerializer.Deserialize<ObservableCollection<Message1>>(datas);
+           
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Messages.Add(new Message1(txtbox.Text, DateTime.Now));
+            SaveMessages();
+        }
+
+        private void SaveMessages()
+        {
+            string messages = JsonSerializer.Serialize(Messages);
+                File.WriteAllText("../../../msg.json", messages);
+            
+        }
+    }
+}
---
+++ WpfApp4/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4;
+
+public
+    class Message1
+{
+
+
+    public Message1() { }
+    public string? _Text { get; set; }
+    public DateTime? _Time { get; set; }
+    public Message1(string text, DateTime time)
+    {
+        _Text = text;
+        _Time = time;
+    }
+
+    public override string ToString()
+    {
+        return $"{_Time.Value.Hour}:{_Time.Value.Minute}  {_Text}";
+    }
+
+}
